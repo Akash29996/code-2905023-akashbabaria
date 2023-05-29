8b85eaf608fe1463e0b65a03f129bef8a73f16b7
Akash29996 <akashbabaria96@gmail.com>
2023-05-29 14:47:50 +0530

    test cases

--- test/bmi.test.js
+++ test/bmi.test.js
@@ -18,6 +18,14 @@ describe('BMI Calculation', () => {
     expect(response).to.deep.equal({category : "Underweight",risk : "Malnutrition risk"});
   });
 
-  
+  it('should calculate bmi for given height and weigth', async () => {
+    const response = contollerBmi.calculation(208, 68)
+    expect(response).to.deep.equal(15.72);
+  });
+
+  it('should calculate bmi for request', async () => {
+    contollerBmi.calculateBMI(req,res1)
+    expect(res1.json.called);
+  });
 
 });
