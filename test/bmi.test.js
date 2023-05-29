7ca8afbc8bcd36f2220e93b08d9bb46ae61c4a25
Akash29996 <akashbabaria96@gmail.com>
2023-05-29 14:45:27 +0530

    documents

--- test/bmi.test.js
+++ test/bmi.test.js
@@ -32,15 +32,11 @@ describe('BMI Calculation', () => {
     json: jest.fn(),
   };
 
-  it('should give cateogry and risk for BMI value', async () => {
-    const response = contollerBmi.calculateBodyHealth(18)
-    expect(response).to.deep.equal({category : "Underweight",risk : "Malnutrition risk"});
-  });
+  
 
-  it('should calculate bmi for given height and weigth', async () => {
-    const response = contollerBmi.calculation(208, 68)
-    expect(response).to.deep.equal(15.72);
+  it('should calculate bmi for request', async () => {
+    contollerBmi.calculateBMI(req,res1)
+    expect(res1.json.called);
   });
-
 
 });
